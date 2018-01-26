0c6f76d90f3ed660016ba395905b5efbd0bb5b52
ryanlazo <ryanlazo12@gmail.com>
2018-01-26 10:17:49 -0500

    FizzbuzzAndroid

---
+++ app/src/main/java/edu/cnm/deepdive/fizzbuzzandroid/FizzBuzzActivity.java
@@ -0,0 +1,32 @@
+package edu.cnm.deepdive.fizzbuzzandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class FizzBuzzActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_fizz_buzz);
+    final TextView text = (TextView) findViewById(R.id.text_view);
+    text.setText(FizzBuzz.count (50));
+
+    final EditText limit = (EditText) findViewById(R.id.limit);
+    Button button = (Button) findViewById(R.id.button);
+    button.setOnClickListener (new OnClickListener(){
+        @Override
+      public void onClick(View v) {
+          text.setText(FizzBuzz.count(Integer.parseInt(limit.getText().toString())));
+        }
+
+    } );
+
+  }
+}
+
